3ce1517ac8654dce6faebe3e2c74f0c094424013
CaoSY <caoshuyang1996@pku.edu.cn>
2018-03-10 21:43:35 +0800

    modify kernel makefile

--- inc/color.h
+++ inc/color.h
@@ -1,7 +1,7 @@
 #ifndef JOS_INC_COLOR_H
 #define JOS_INC_COLOR_H
 
-extern int textcolor
+extern int textcolor;
 
 #define DF_TEXT_COLOR 0x0700            // defualt text color, white (low density) foreground and black background
 
