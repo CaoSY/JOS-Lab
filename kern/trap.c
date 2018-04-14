5c977a588fb09c285b7d96d81fd5aed4380d4784
CaoSY <caoshuyang1996@pku.edu.cn>
2018-04-14 21:03:57 +0800

    lab 3 exercise 8 done

--- kern/trap.c
+++ kern/trap.c
@@ -164,12 +164,13 @@ trap_dispatch(struct Trapframe *tf)
 					  return;
 		case T_PGFLT: page_fault_handler(tf);
 					  return;
-		case T_SYSCALL: syscall(tf->tf_regs.reg_eax,
-								tf->tf_regs.reg_edx,
-								tf->tf_regs.reg_ecx,
-								tf->tf_regs.reg_ebx,
-								tf->tf_regs.reg_edi,
-								tf->tf_regs.reg_esi);
+		case T_SYSCALL: tf->tf_regs.reg_eax = syscall(
+									tf->tf_regs.reg_eax,
+									tf->tf_regs.reg_edx,
+									tf->tf_regs.reg_ecx,
+									tf->tf_regs.reg_ebx,
+									tf->tf_regs.reg_edi,
+									tf->tf_regs.reg_esi);
 					  return;
 	}
 
