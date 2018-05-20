e6f62663eac1f9afa61540172a4aa943a7ca8752
CaoSY <caoshuyang1996@pku.edu.cn>
2018-05-21 00:05:45 +0800

    fix a typo in sfork.c

--- user/sfork.c
+++ user/sfork.c
@@ -7,10 +7,10 @@ void umain(int argc, char **argv)
     int ch = sfork ();
     
     if (ch != 0) {
-        cprintf ("I’m parent with share num = %d\n", share);
+        cprintf ("I'm parent with share num = %d\n", share);
         share = 2;
     } else {
         sys_yield();
-        cprintf ("I’m child with share num = %d\n", share);
+        cprintf ("I'm child with share num = %d\n", share);
     }
 }
